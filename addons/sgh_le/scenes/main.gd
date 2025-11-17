298845af692b41e2049bea763fbceb5cb05754ed
mFieldHouses <mathias.veldhuizen@gmail.com>
2025-11-17 18:44:50 +0100

    add Level node class

--- addons/sgh_le/scenes/main.gd
+++ addons/sgh_le/scenes/main.gd
@@ -1,7 +1,7 @@
 @tool
 extends Control
 
-var loaded_scene_path : String = ""
+var loaded_scene_path : String = "a"
 var loaded_room_name : String
 
 # Called when the node enters the scene tree for the first time.
@@ -11,7 +11,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	$main/not_a_level_warning.visible = EditorInterface.get_edited_scene_root() is not Level
+
+		
 
 func _show_screen(control_name : String) -> void:
 	for _control in get_children():
