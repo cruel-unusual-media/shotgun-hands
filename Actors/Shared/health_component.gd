665c1af439735a4439093651e93a15042155c955
Ansterar <Abellingesq@gmail.com>
2025-11-22 10:31:15 +1100

    Added more components
     - Damagable component, for things that need to be able to be hit. Instantiate the scene to use it as otherwise the signals will be disconnected.
     - health component, for things with an amount of health
     - attack area, a damaging hitbox
     - shooting component (class only, no scene)

---
+++ Actors/Shared/health_component.gd
@@ -0,0 +1,28 @@
+class_name HealthComponent extends Node2D
+
+@export
+var health_max : int = 10
+var health_cur : int
+@export
+var health_bar : ProgressBar
+signal on_death
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func take_damage(damage : int) -> void:
+	health_cur -= damage
+	health_cur = clamp(health_cur, 0, health_max)
+	if health_cur <= 0:
+		on_death.emit()
+
+func recover_health(healed : int) -> void:
+	health_cur += healed
+	health_cur = clamp(health_cur, 0, health_max)
+	
---
+++ damagable_component.gd
@@ -0,0 +1,23 @@
+class_name DamagableComponent extends Area2D
+
+signal hit_for(damage_amount : int)
+@export
+var health_component : HealthComponent
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_area_entered(area: Area2D) -> void:
+	pass
+
+func get_hit(damage):
+	hit_for.emit(damage)
+	health_component.take_damage(damage)
