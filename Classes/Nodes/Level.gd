e815da0003b037030f6551780f2629791612225d
mFieldHouses <mathias.veldhuizen@gmail.com>
2025-11-21 21:18:54 +0100

    Update Level.gd

--- Classes/Nodes/Level.gd
+++ Classes/Nodes/Level.gd
@@ -6,14 +6,23 @@ class_name Level
 #A lot of these values are @exports. This is because only exported values get saved when packing a scene. Thus, if we want the level editor to be able to store information, we need exports
 
 @export var _level_start : LevelStart #set by the level editor
-
+#
 func _ready() -> void:
-	_level_start = $room1/LevelStart #ONLY HERE FOR DEBUGGING LEVEL, ONCE REAL LEVELS HAVE BEEN IMPLEMENTED THIS NEEDS TO GO
+	initialize_level()
+	#_level_start = $room1/LevelStart #ONLY HERE FOR DEBUGGING LEVEL, ONCE REAL LEVELS HAVE BEEN IMPLEMENTED THIS NEEDS TO GO
 
 
 func initialize_level() -> void:
 	for _room : LevelRoom in get_children():
-		pass
+		var _has_level_start : bool = false
+		
+		for _child in _room.get_node("main").get_children():
+			if _child is LevelStart:
+				_level_start = _child
+				_has_level_start = true
+				break
+		
+		_freeze_room(_room, !_has_level_start)
 	
 	var _new_player_instance = preload("res://Actors/Player/player.tscn").instantiate()
 	_level_start.add_sibling(_new_player_instance)
@@ -35,3 +44,14 @@ func enter_room(room_to_enter : LevelRoom, entrance : RoomEntrance) -> void:
 		else:
 			_room.process_mode = Node.PROCESS_MODE_DISABLED
 			_room.visible = false
+
+
+func _freeze_room(room : LevelRoom, state : bool) -> void:
+	room.visible = !state
+	match state:
+		false:
+			room.position.y = 0
+			room.process_mode = Node.PROCESS_MODE_ALWAYS
+		true:
+			room.position.y = 10000
+			room.process_mode = Node.PROCESS_MODE_DISABLED
