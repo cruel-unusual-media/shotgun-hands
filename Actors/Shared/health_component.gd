c17cd1b284f95a2fea60cc15dd3b8923933c9b0e
Ansterar <Abellingesq@gmail.com>
2025-11-22 11:07:18 +1100

    More work

--- Actors/Shared/health_component.gd
+++ Actors/Shared/health_component.gd
@@ -23,6 +23,7 @@ func take_damage(damage : int) -> void:
 	health_cur = clamp(health_cur, 0, health_max)
 	if health_cur <= 0:
 		on_death.emit()
+	print("I was hit for " + str(damage) + " Damage")
 
 func recover_health(healed : int) -> void:
 	health_cur += healed
