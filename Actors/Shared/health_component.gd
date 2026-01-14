a9bef27487a1bf09bc0342bfbfaca27389670b56
Aidan Belling <abellingesq@gmail.com>
2026-01-14 11:50:48 +1100

    Fixed Health component bug
    Fixed bug where health component would initialise with 0 health

--- Actors/Shared/health_component.gd
+++ Actors/Shared/health_component.gd
@@ -9,7 +9,7 @@ signal on_death
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	health_cur = health_max
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
